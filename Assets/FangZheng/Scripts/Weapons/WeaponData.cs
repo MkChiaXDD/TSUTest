43a5f7fc4e79a01f40bc68692a828354cbc368c6
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-06-30 17:55:42 +0800

    RemoveFiles

--- Assets/FangZheng/Scripts/Weapons/WeaponData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Weapons/Weapon Data")]
-public class WeaponData : ScriptableObject
-{
-    public string weaponName;
-    public int damage;
-    public LayerMask hitLayers;
-}
